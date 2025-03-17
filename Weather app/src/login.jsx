76af9b022e5faaed2d92742b7c0c676cfed5af2f
Ola <hollabodey.kilaso@gmail.com>
2025-03-17 15:37:34 +0000

    Finalised login design

--- Weather app/src/login.jsx
+++ Weather app/src/login.jsx
@@ -1,11 +1,17 @@
 import './Login.css'
 import google_icon from "./assets/google.svg"
 import facebook_icon from "./assets/facebook.svg"
+import logo from "/public/rainy-day.png"
 
 function Login() {
     return (
         <>
             <div className='card'>
+                <div className='weatherLogo'>
+                    <img src={logo} alt="logo"/>
+                    <h2>Weatherly</h2>
+                </div>
+
                 <h1>Sign in</h1>
 
                 <div className='inputs'>
@@ -17,10 +23,25 @@ function Login() {
                 </div>
 
                 <button>Sign in</button>
-            </div>
 
-            <img src={google_icon} alt="" />
-            <img src={facebook_icon} alt="" />
+                <div className="divider">or</div> 
+
+                <div className='altOptions'>
+                    <button>
+                        <img src={google_icon} alt="google logo" />
+                        Sign in with google
+                    </button>
+
+                    <button>
+                        <img src={facebook_icon}  alt="facebook logo"/>
+                        Sign in with facebook
+                    </button>
+                    
+                </div>
+
+                <h3>Dont have an account? <span><a href="#">Sign Up</a></span></h3>
+
+            </div>
         
         </>
     )

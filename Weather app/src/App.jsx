8fe4679b710ffbd30db55aa843a684df1d1ce344
Ola <hollabodey.kilaso@gmail.com>
2025-03-17 09:37:31 +0000

    Restructured the code

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -13,33 +13,37 @@ import Axios from 'axios';
 
 
 function App() {
-  // let ApiKey = "278b506dd020c8ea4a386833fc15488d"   // Add you api key here
+  // let ApiKey = "278b506dd020c8ea4a386833fc15488d"  
+  let city = "London"
+
+  // let ApiKey = "278b506dd020c8ea4a386833fc15488d"   //* Uncomment & Add your api key here
 
   const [weatherDetails, setweatherDetails] = useState(null)
 
   const fetchData =() => {
-    Axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${ApiKey}`).then((res) => {
-      setweatherDetails(res.data)
-      // console.log(res.data)
+    Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`).then((res) => {
+      setweatherDetails(res.data);
+      console.log(res.data)
     })
   
   }
-
-  
-
 
   return (
     <>
       <button onClick={fetchData} > Get Location</button>
         <div className='details'>
-          <h2>Location : London</h2>
+          <h2>Getting the weather for {city}</h2>
+
+          {/* <h2>Location : {weatherDetails?.city}</h2> */}
           <h2>Temp : {weatherDetails?.main.temp}</h2>
           <h2>Pressure : {weatherDetails?.main.pressure}</h2>
           <h2>feels Like : {weatherDetails?.main.feels_like}</h2>
+          <h2>description : {weatherDetails?.weather[0].description}</h2>
         </div>
 
     </>
   )
+
 }
 
 export default App

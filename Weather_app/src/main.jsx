05775242aedacb86a66df172dcb17bcce8467566
Ola <hollabodey.kilaso@gmail.com>
2025-03-24 11:46:53 +0000

    Add sign-up page component and update import path in main.jsx

--- Weather_app/src/main.jsx
+++ Weather_app/src/main.jsx
@@ -1,27 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// // import App from './App.jsx'
-// import Login from './login.jsx'
-
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-
-//     {/* <Login/> */}
-
-//   </StrictMode>,
-// )
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './index.css'
 import App from './App.jsx'
 import Login from './login.jsx'
-import SignUp from './signIn.jsx'
+import SignUp from '../src/pages/signUp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

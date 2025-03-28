c8e32a8793a937c5f1738a7c1e0c10a53354dd0c
Ola <hollabodey.kilaso@gmail.com>
2025-03-28 14:20:01 +0000

    Update App component layout and enhance styling for better presentation

--- Weather_app/src/App.jsx
+++ Weather_app/src/App.jsx
@@ -77,7 +77,12 @@ export default function App (){
 
         <div className="second--div">
           <div className="layout">
-
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
           </div>
         </div>
       </div>

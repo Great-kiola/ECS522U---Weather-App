1cbb4f9fa4c0efba992f795f07c5f1d1b4fa23db
XxUmerKhanxX <158104662+XxUmerKhanxX@users.noreply.github.com>
2025-03-31 23:31:48 +0100

    Update login.jsx

--- Weather_app/src/login.jsx
+++ Weather_app/src/login.jsx
@@ -3,15 +3,15 @@ import React, { useState, useEffect } from 'react';
 
 // Firebase Imports
 import { auth } from './firebase'; // confirm path
-import {
+import { 
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   FacebookAuthProvider,
-  signInWithPopup
+  signInWithPopup 
 } from 'firebase/auth';
 
 // Other imports
-import '../src/styling/Login.css';
+import './Login.css';
 import google_icon from './assets/google.svg';
 import facebook_icon from './assets/facebook.svg';
 import logo from '/rainy-day.png';
@@ -20,17 +20,17 @@ import { Link } from 'react-router-dom';
 export default function Login() {
   const initialForm = {
     email: "",
-    password: ""
+    password: "" 
   };
 
-
+  // State management
   const [form, setForm] = useState(initialForm);
   const [formErr, setFormErr] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
+    const {name, value} = e.target;
+    setForm({...form, [name]: value});
   };
 
   const handleSubmit = (e) => {
@@ -79,13 +79,13 @@ export default function Login() {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!value.password) {
+    if(!value.password){
       errors.password = "Password is required";
     }
 
-    if (!value.email) {
+    if(!value.email){
       errors.email = "Email is required";
-    } else if (!regex.test(value.email)) {
+    } else if(!regex.test(value.email)) {
       errors.email = "Email is invalid";
     }
 
@@ -100,74 +100,69 @@ export default function Login() {
 
   return (
     <>
-      <div className="masterDiv">
-        <div className='card'>
-          <div className='weatherLogo'>
-            <img src={logo} alt="logo" />
-            <h2>Weatherly</h2>
-          </div>
+      <div className='card'>
+        <div className='weatherLogo'>
+          <img src={logo} alt="logo" />
+          <h2>Weatherly</h2>
+        </div>
 
-          <h1 className='title'>Welcome Back!</h1>
+        <h1 className='title'>Welcome Back!</h1>
 
-          <form onSubmit={handleSubmit}>
-            <div className='inputs'>
-              <div className='infoBar'>
-                <label>Email</label>
-                <p className={formErr.email ? 'err' : ''}>{formErr.email}</p>
-              </div>
-
-              <input
-                type="email"
-                placeholder='******@anymail.com'
-                className={formErr.email ? 'inputErr' : ''}
-                name="email"
-                value={form.email}
-                onChange={handleChange}
-              />
-
-              <div className='infoBar'>
-                <label>Password</label>
-                <p className={formErr.password ? "err" : ''}> {formErr.password}</p>
-              </div>
-
-              <input
-                type="password"
-                placeholder='Enter your password'
-                className={formErr.password ? 'inputErr' : ''}
-                name="password"
-                value={form.password}
-                onChange={handleChange}
-              />
+        <form onSubmit={handleSubmit}>
+          <div className='inputs'>
+            <div className='infoBar'>
+              <label>Email</label>
+              <p className={formErr.email ? 'err' : ''}>{formErr.email}</p>
             </div>
 
-            <button type='submit'> Sign in </button>
-            {formErr.auth && <p className='err'>{formErr.auth}</p>}
-          </form>
+            <input 
+              type="email" 
+              placeholder='******@anymail.com' 
+              className={formErr.email ? 'inputErr' : ''}
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+            />
 
-          <div className="divider">or</div>
+            <div className='infoBar'>
+              <label>Password</label>
+              <p className={formErr.password ? "err" : ''}> {formErr.password}</p>
+            </div>
 
-          <div className='altOptions'>
-            <button onClick={googleSignIn}>
-              <img src={google_icon} alt="google logo" />
-              Sign in with Google
-            </button>
-
-            <button onClick={facebookSignIn}>
-              <img src={facebook_icon} alt="facebook logo" />
-              Sign in with Facebook
-            </button>
+            <input 
+              type="password" 
+              placeholder='Enter your password' 
+              className={formErr.password ? 'inputErr' : ''}
+              name="password"
+              value={form.password} 
+              onChange={handleChange}
+            />
           </div>
 
-          <h3>Don't have an account?
-            <span>
-              <Link to="/signup">Sign Up</Link>
-            </span>
-          </h3>
-        </div>
-      </div>
+          <button type='submit'> Sign in </button>
+          {formErr.auth && <p className='err'>{formErr.auth}</p>}
+        </form>
 
+        <div className="divider">or</div>
+
+        <div className='altOptions'>
+          <button onClick={googleSignIn}>
+            <img src={google_icon} alt="google logo" />
+            Sign in with Google
+          </button>
+
+          <button onClick={facebookSignIn}>
+            <img src={facebook_icon} alt="facebook logo" />
+            Sign in with Facebook
+          </button>
+        </div>
+
+        <h3>Don't have an account? 
+          <span>
+            <Link to="/signup">Sign Up</Link>
+          </span>
+        </h3>
+      </div>
     </>
   );
 }
-
-
